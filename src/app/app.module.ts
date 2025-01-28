cac9cb53840f5ddc821dc3e4c25f496eb9afcf35
Alayssa <07212306@gmail.com>
2025-01-28 17:53:42 +0800

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts /post-create/post.create.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+
+
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
