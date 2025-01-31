a780f3b470a76fbb2382d26b73cb3c8aedca4fbf
Alayssa <07212306@gmail.com>
2025-01-31 17:52:05 +0800

    ABranch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts /post-create/post.create.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts /post-list/post.list.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -23,8 +31,10 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatFormFieldModule,
     MatButtonModule,
-
-
+    MatToolbarModule,
+    MatExpansionModule,
+    MatDividerModule,
+    
       ],
   providers: [],
   bootstrap: [AppComponent]
